4af378d5b44bea8c685bc41d2ec2767c1d4f644e
jyxjjj <773933146@qq.com>
2025-03-22 17:51:41 +0800

    2025-03-22 17:51:41

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,28 @@
+import React from 'react';
+
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-createRoot(document.querySelector('root')!).render(<App />);
+import theme from './theme';
+
+import './global.css';
+
+import Home from './Pages/Home';
+
+createRoot(document.querySelector('root')!)
+    .render(
+        <React.StrictMode>
+            <ThemeProvider
+                theme={theme}
+                defaultMode='system'
+                modeStorageKey='theme'
+                colorSchemeStorageKey='colorScheme'
+                noSsr
+            >
+                <CssBaseline enableColorScheme />
+                <Home />
+            </ThemeProvider>
+        </React.StrictMode>
+    );
