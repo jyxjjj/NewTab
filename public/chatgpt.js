73fdf323b4601965096334acd530591e7fd2d5ab
jyxjjj <773933146@qq.com>
2025-12-09 15:51:30 +0800

    2025-12-09 15:51:30

--- public/chatgpt.js
+++ public/chatgpt.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     const css = `
-        div[class*="--thread-content-max-width:40rem"][class*="thread-lg:[--thread-content-max-width:48rem]"] {
+        #thread > div > div.relative.basis-auto.flex-col.-mb-\(--composer-overlap-px\).\[--composer-overlap-px\:28px\].grow.flex.overflow-hidden > div > div > div.flex.flex-col.text-sm.\@w-xl\/main\:pt-header-height.pb-25 > article.text-token-text-primary.w-full.focus\:outline-none.\[--shadow-height\:45px\].has-data-writing-block\:pointer-events-none.has-data-writing-block\:-mt-\(--shadow-height\).has-data-writing-block\:pt-\(--shadow-height\).\[\&\:has\(\[data-writing-block\]\)\>\*\]\:pointer-events-auto.\[content-visibility\:auto\].supports-\[content-visibility\:auto\]\:\[contain-intrinsic-size\:auto_100lvh\].scroll-mt-\[calc\(var\(--header-height\)\+min\(200px\,max\(70px\,20svh\)\)\)\] > div > div {
             max-width: 100% !important;
         }
     `;
